1555aad4c05ff51a6de3d41e304498b65f6efb16
Ali Raheem <ali.raheem@gmail.com>
2018-01-18 02:22:07 +0000

    Added recent orders to user page, cleaned up pug

--- app.js
+++ app.js
@@ -1,71 +1,41 @@
-var express = require('express');
-var pug = require('pug');
+const express = require('express');
+const pug = require('pug');
+const expressMongoDb = require('express-mongo-db');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const dbUrl = 'mongodb://localhost:27018';
-const dbName = 'rtb-db';
-const ordersTable = 'orders';
-const usersCol = "users";
-
-
-
-async function dbGetOrders() {
-  let client;
-
-  try {
-      client = await MongoClient.connect(dbUrl);
-      
-      const db = client.db(dbName);
-      const col = db.collection(ordersTable);
-
-      var orders = await col.find({}).limit(100).sort({"_id": -1}).toArray();
-      return orders;
-  } catch (err) {
-      console.log(err.stack);
-  }
-      client.close();
-};
-
-async function dbFindUser(name) {
-  let client;
-
-  try {
-      client = await MongoClient.connect(dbUrl);
-      
-      const db = client.db(dbName);
-      const col = db.collection(usersCol);
-
-      var user = await col.findOne({"name": name});
-      return user;
-  } catch (err) {
-      console.log(err.stack);
-  }
-      client.close();
-};
-
 var app = express();
-
 app.set('view engine', 'pug');
 app.set('views', './views');
-
 app.use(express.static('pub'));
+app.use(expressMongoDb('mongodb://localhost:27018/rtb-db'));
+
+const ordersCol = "orders";
+const usersCol = "users";
 
 app.get('/', (req, res) => {
     res.render('index', {});
 });
 
 app.get('/orders', async function (req, res) {
-    var orders = await dbGetOrders();
+    var name = req.params.name;
+    var orders = await req.db.collection(ordersCol).find({}).limit(100).sort({"_id": -1}).toArray();
     res.render('orders', {'orders': orders});
 });
 
 app.get('/user/:name', async (req, res) => {
-    user = await dbFindUser(req.params.name);
+    var name = req.params.name;
+    var user = await req.db.collection(usersCol).findOne({"name": name})
     if(user == null) {
 	res.status(500).send("User does not exist.");
     } else {
-	res.render('user', {'user': user, 'coins': user.coins});
+	var orders = req.db.collection(ordersCol);
+	var fromOrders = await orders.find({"from": name}).limit(10).toArray();
+	var toOrders = await orders.find({"to": name}).limit(10).toArray();
+	res.render('user', {'user': user,
+			    'coins': user.coins,
+			    'fromOrders': fromOrders,
+			    'toOrders': toOrders});
     }
 });
 
